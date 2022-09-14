f1d6d0589f300cdc1299e92c3fa7250c153c8892
pklm360 <raphaelpklm.2@gmail.com>
2022-09-14 21:27:09 +0200

    documentation du fichier decode

--- decodeFinal.py
+++ decodeFinal.py
@@ -1,13 +1,15 @@
 import PIL.Image
 
+### Entree et verification de l'image ###
 filePic = 'out'
+
 
 img = PIL.Image.open(filePic + '.png')
 largeur, hauteur = img.size
 
 
 
-
+### Recuperation des valeurs RVB de chaque pixels ###
 binPixels = []
 for y in range(hauteur):
     for x in range(largeur):
@@ -15,17 +17,18 @@ for y in range(hauteur):
         for i in range(3):
             binPixels.append(bin(rvb[i]))
 
-
+### Decodage de la longeur du message ###
 length = ''
 for i in range(15):
     length += binPixels[i][-1]
 
 length = int(length, 2)
 
+### Decodage des lettres ###
 binLetters = []
 letter = ''
 
-for i in range(15, 15+length*8):
+for i in range(15, 15+length*8): #On commence le decodage à l'indice 15
 
     if len(letter) == 8:
         binLetters.append(chr(int(letter, 2)))
